a3d335d94a2c133b1faf554f663cad29a08a2188
EdwinZarateP <emzp1994@gmail.com>
2024-08-19 22:53:40 -0500

    inicio del proyecto

--- src/Paginas/Inicio/index.tsx
+++ src/Paginas/Inicio/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Importa el componente Link
 import './estilos.css';
 import logo from '../../Imagenes/logo.jpeg'; 
 
 const Inicio: React.FC = () => {
     return (
-        <div>
+        <div className='ContenedorInicio'>
             <h1>Hola soy inicio</h1>
+            
+            <Link to="/registro">
+                <button className="botonRegistro">Ir a Registro</button>
+            </Link>
+
             <img src={logo} alt="Logo iglesia" className="logo" />
         </div>
     );
